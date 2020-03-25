aad07561cd0d6eb723a379bef47b2630ea66dc96
Muhammad Khoirul Huda <khairulhuda@ruangguru.com>
2020-03-25 23:34:00 +0700

    [master] show signing

--- src/app.ts
+++ src/app.ts
@@ -37,7 +37,7 @@ slackEvents.on("error", error => {
     console.error(`An unverified request was sent to the Slack events Request URL. Request body: \
 ${JSON.stringify(error.body)}`);
   } else {
-    console.log(`node_env`, process.env.NODE_ENV);
+    console.log(`node_env`, process.env.NODE_ENV, slackSigningSecret);
     console.error(
       `An error occurred while handling a Slack event: ${error.message}`,
       error
